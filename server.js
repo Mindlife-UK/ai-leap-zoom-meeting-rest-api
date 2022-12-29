7cc79989f3982bb8c8a6543c679220df381efbbe
Kev <numerized@users.noreply.github.com>
2022-12-29 17:54:27 +0100

    zoom oauth

--- server.js
+++ server.js
@@ -14,7 +14,28 @@ app.use(compression()); //Compress all routes
 app.use(cors({ origin: "*" }));
 
 app.get("/oauth", (req, res, next) => {
- res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+
+  const httpOptions2 = {
+    headers: new HttpHeaders({
+      Authorization:
+        "Basic RWRSN3JXUklSd0dkUXlxaG9YUGcydzpqN09SUUo2dHlIc2gyM215Y2hDMkFPcmRLb3RZcTN2Mw==",
+      "Content-Type": "application/x-www-form-urlencoded"
+    })
+  };
+  res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
+
+  axios
+    .post(
+      "https://zoom.us/oauth/token?grant_type=authorization_code&code=" +
+        params.code +
+        "&redirect_uri=http://localhost:4200/protocols/create",
+      {},
+      httpOptions2
+    )
+    .then((authRes) => {
+      console.log(authRes);
+      // this.addZoomMeeting();
+    });
 });
 
 app.listen(process.env.PORT || 3000);
