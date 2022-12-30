d80257c94a8cf56aced56eaa7f71d70b2e6fa7ad
Kev <numerized@users.noreply.github.com>
2022-12-30 16:59:06 +0100

    axios post inline

--- server.js
+++ server.js
@@ -53,37 +53,30 @@ app.post("/users/:access_token", (req, res) => {
   console.log("heroku users");
 
   const options = {
-    method: "POST",
-    hostname: "api.zoom.us",
-    port: null,
-    path: `/v2/users?access_token=${req.params.access_token}`,
+    method: "post",
+    url: `/v2/users?access_token=${req.params.access_token}`,
     headers: {
       "content-type": "application/json",
     },
-  };
-
-  const data = {
-    action: "create",
-    user_info: {
-      email: "jchill@example.com",
-      first_name: "Jill",
-      last_name: "Chill",
-      // password: "if42!LfH@",
-      type: 1,
-      feature: {
-        zoom_phone: true,
-        zoom_one_type: 16,
+    data = {
+      action: "create",
+      user_info: {
+        email: "jchill@example.com",
+        first_name: "Jill",
+        last_name: "Chill",
+        // password: "if42!LfH@",
+        type: 1,
+        feature: {
+          zoom_phone: true,
+          zoom_one_type: 16,
+        },
+        plan_united_type: "1",
       },
-      plan_united_type: "1",
-    },
-  };
+    }
+  }; 
 
   axios
-    .post(
-      `https://zoom.us/v2/users`,
-      JSON.stringify(data),
-      options
-    )
+    .post(options)
     .then((result) => {
       console.log(`Status: ${result.status}`);
       console.log("Body: ", result.data);
