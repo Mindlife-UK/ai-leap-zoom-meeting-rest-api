ca07cf940dbda17bd52d3b600a8726a5c87a48fb
Kev <numerized@users.noreply.github.com>
2022-12-30 16:33:17 +0100

    adding user creation

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get("/oauth/:code", (req, res, next) => {
   console.log(req);
   console.log("heroku oauth");
 
-  const httpOptions2 = {
+  const httpOptions = {
     headers: {
       Authorization:
         "Basic RWRSN3JXUklSd0dkUXlxaG9YUGcydzpqN09SUUo2dHlIc2gyM215Y2hDMkFPcmRLb3RZcTN2Mw==",
@@ -35,7 +35,54 @@ app.get("/oauth/:code", (req, res, next) => {
         req.params.code +
         "&redirect_uri=http://localhost:4200/protocols/create",
       { code: req.params.code },
-      httpOptions2
+      httpOptions
+    )
+    .then((result) => {
+      console.log(`Status: ${result.status}`);
+      console.log("Body: ", result.data);
+      res.json(result.data);
+    })
+    .catch((err) => {
+      console.error(err);
+      throw new Error(err);
+    });
+});
+
+app.post("/users/:access_token", (req, res, next) => {
+  console.log(req.data);
+  console.log("heroku users");
+
+  const options = {
+    method: "POST",
+    hostname: "api.zoom.us",
+    port: null,
+    path: `/v2/users?access_token=${token}`,
+    headers: new HttpHeaders({
+      "content-type": "application/json"
+    })
+  };
+
+  const data = {
+    action: "create",
+    user_info: {
+      email: "jchill@example.com",
+      first_name: "Jill",
+      last_name: "Chill",
+      password: "if42!LfH@",
+      type: 1,
+      feature: {
+        zoom_phone: true,
+        zoom_one_type: 16
+      },
+      plan_united_type: "1"
+    }
+  };
+
+  axios
+    .post(
+      `https://zoom.us/v2/users?access_token=${token}`,
+      JSON.stringify(data),
+      options
     )
     .then((result) => {
       console.log(`Status: ${result.status}`);
