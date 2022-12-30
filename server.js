7d8b7a73695fc73c6a1229e4c28608b335a97ad2
Kev <numerized@users.noreply.github.com>
2022-12-30 23:58:17 +0100

    refactoring axios requests

--- server.js
+++ server.js
@@ -19,56 +19,30 @@ app.get("/oauth/:code", (req, res, next) => {
   // console.log(req);
   console.log("heroku oauth");
 
-  const httpOptions = {
+  const axiosOptions = {
+    baseURL: "https://zoom.us",
+    method: "get",
+    url: `oauth/token?grant_type=authorization_code&code=${req.params.code}&redirect_uri=http://localhost:4200/protocols/create`,
     headers: {
-      Authorization:
-        "Basic RWRSN3JXUklSd0dkUXlxaG9YUGcydzpqN09SUUo2dHlIc2gyM215Y2hDMkFPcmRLb3RZcTN2Mw==",
-      authorization:
-        "Basic RWRSN3JXUklSd0dkUXlxaG9YUGcydzpqN09SUUo2dHlIc2gyM215Y2hDMkFPcmRLb3RZcTN2Mw==",
-      "Content-Type": "application/x-www-form-urlencoded",
+      headers: {
+        Authorization:
+          "Basic RWRSN3JXUklSd0dkUXlxaG9YUGcydzpqN09SUUo2dHlIc2gyM215Y2hDMkFPcmRLb3RZcTN2Mw==",
+        authorization:
+          "Basic RWRSN3JXUklSd0dkUXlxaG9YUGcydzpqN09SUUo2dHlIc2gyM215Y2hDMkFPcmRLb3RZcTN2Mw==",
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
     },
+    data: { code: req.params.code },
   };
 
-  axios
-    .post(
-      "https://zoom.us/oauth/token?grant_type=authorization_code&code=" +
-        req.params.code +
-        "&redirect_uri=http://localhost:4200/protocols/create",
-      { code: req.params.code },
-      httpOptions
-    )
-    .then((result) => {
-      // console.log(`Status: ${result.status}`);
-      // console.log("Body: ", result.data);
-      res.json(result.data);
-    })
-    .catch((err) => {
-      // console.error(err);
-      throw new Error(err);
-    });
+  sendAxiosRequest(axiosOptions);
 });
 
 app.post("/users/:access_token", (req, res) => {
   console.log(req.data);
   console.log("heroku users");
 
-  const data = {
-    action: "create",
-    user_info: {
-      email: "kevin@help.org.uk",
-      first_name: "Jill",
-      last_name: "Chill",
-      password: "if42!LfH@",
-      type: 1,
-      feature: {
-        zoom_phone: false,
-        // zoom_one_type: 16,
-      },
-      // plan_united_type: "1",
-    },
-  };
-
-  const options = {
+  const axiosOptions = {
     baseURL: "http://api.zoom.us",
     method: "post",
     url: `/v2/users`,
@@ -76,14 +50,26 @@ app.post("/users/:access_token", (req, res) => {
       Authorization: `BEARER ${req.params.access_token}`,
       "content-type": "application/json",
     },
-    // params: {
-    //   access_token: req.params.access_token
-    // },
-    data: data,
+    data: {
+      action: "create",
+      user_info: {
+        email: "kevin@help.org.uk",
+        first_name: "Jill",
+        last_name: "Chill",
+        password: "if42!LfH@",
+        type: 1,
+        feature: {
+          zoom_phone: false,
+        },
+      },
+    },
   };
+  sendAxiosRequest(axiosOptions);
+});
 
+sendAxiosRequest = (axiosOptions) => {
   axios
-    .post(`http://api.zoom.us//v2/users`, data, options)
+    .post(axiosOptions)
     .then((result) => {
       console.log(result);
       console.log(`Status: ${result.status}`);
@@ -94,6 +80,6 @@ app.post("/users/:access_token", (req, res) => {
       console.error(err);
       throw new Error(err);
     });
-});
+};
 
 app.listen(process.env.PORT || 3000);
